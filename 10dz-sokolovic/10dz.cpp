9adb38d957847d90a087130a558497443d6da47e
markosokolovic7 <msokolovic21@gmail.com>
2017-06-05 21:22:29 +0200

    popravio sam dio

--- 10dz-sokolovic/10dz.cpp
+++ 10dz-sokolovic/10dz.cpp
@@ -4,21 +4,30 @@ using namespace std;
 
 int main()
 {
-    ifstream artikli;
-      string artikl;
-    int n,cijena;
+    ofstream artikli;
+    int n;
 
     cout<<"Upisite koliko ce te artikla unijeti: ";
     cin>>n;
+    string artikl[];
+    int cijena[];
+    datoteka.open("artikli.txt", ios::app);
+    while(1)
+    {
+        //!Profesore ja kad otvorim 13 vjeznu meni nema onog koda kj tream stavit tu!
+        if(datoteka.eof()==true)
+            break;
+    }
     for(int i=0;i<n;i++)
     {
         cout<<"Unesi ime artikla ";
-    getline(cin,artikl);
+    getline(cin,artikl[i]);
         cout<<endl<<artikl;
         artikli.txt>>artikl;
         cout<<"Cijena artikla: "<<endl;
-        cin>>cijena;
+        cin>>cijena[i];
 
+        datoteka<<artikl<<endl<<cijena<<endl;
     }
 
     return 0;
