f2fd59f1e54b98e60e851611167d45704a917a8b
markosokolovic7 <msokolovic21@gmail.com>
2017-06-07 23:52:26 +0200

    profesore nece nes

--- 10dz-sokolovic/10dz.cpp
+++ 10dz-sokolovic/10dz.cpp
@@ -9,14 +9,27 @@ int main()
 
     cout<<"Upisite koliko ce te artikla unijeti: ";
     cin>>n;
-    string artikl[];
-    int cijena[];
+    string artikl[n];
+    int cijena[n];
     datoteka.open("artikli.txt", ios::app);
-    while(1)
+
+
+         while ( 1 )
     {
-        //!Profesore ja kad otvorim 13 vjeznu meni nema onog koda kj tream stavit tu!
+        datoteka >>artikl[n];
+        if( datoteka.eof() == true )
+            break;
+        datoteka.ignore();
+        getline(datoteka, artikl[n]);
+        datoteka >> cijena[n];
+        n++;
+
+
         if(datoteka.eof()==true)
             break;
+
+         datoteka.close();
+
     }
     for(int i=0;i<n;i++)
     {
